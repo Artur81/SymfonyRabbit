9ce6b95ee02459139600bc6c67692e834822b779
Artur Wasik <arturwasik@o2.pl>
2018-07-31 15:59:36 +0200

    email log added

--- src/Consumer/EmailService.php
+++ src/Consumer/EmailService.php
@@ -1,20 +1,33 @@
 <?php
 namespace App\Consumer;
 
+use App\Entity\Email;
+use Doctrine\ORM\EntityManagerInterface;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class EmailService implements ConsumerInterface
 {
+    /**
+     * @var ContainerInterface
+     */
     private $container;
 
     /**
-     * @param ContainerInterface $container
+     * @var EntityManagerInterface
      */
-    public function __construct(ContainerInterface $container)
+    private $entityManager;
+
+    /**
+     * EmailService constructor.
+     * @param ContainerInterface $container
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(ContainerInterface $container, EntityManagerInterface $entityManager)
     {
         $this->container = $container;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -23,8 +36,12 @@ class EmailService implements ConsumerInterface
      */
     public function execute(AMQPMessage $msg) : int
     {
+        $message = unserialize($msg->body);
+
+        $this->logEmail($message);
+
         $transport = $this->getTransport();
-        $transport->send(unserialize($msg->body));
+        $transport->send($message);
         $transport->stop();
 
         return ConsumerInterface::MSG_ACK;
@@ -43,5 +60,20 @@ class EmailService implements ConsumerInterface
         }
 
         return $swiftTransport;
+    }
+
+    /**
+     * @param \Swift_Mime_SimpleMessage $message
+     */
+    protected function logEmail(\Swift_Mime_SimpleMessage $message) : void
+    {
+        $emailLog = new Email();
+        $emailLog->setEmail(implode(',',array_keys($message->getTo())));
+        $emailLog->setTitle($message->getSubject());
+        $emailLog->setContent($message->getBody());
+        $emailLog->setCreatedAt(new \DateTime());
+
+        $this->entityManager->persist($emailLog);
+        $this->entityManager->flush();
     }
 }
